db58865287ff326e87b4806253d3ef16db1da210
pixelbushi <opiyosbenji@gmail.com>
2025-05-08 04:26:59 +0300

    added error async handlers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import session from 'cookie-session';
 import { config } from './config/app.config';
 import connectDatabase from './config/database.config';
 import { HTTPSTATUS } from './config/http.config';
+import { asyncHandler } from './middlewares/asynchandler.middleware';
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -31,15 +32,19 @@ app.use(
 	})
 );
 
-app.get(`/`, (req: Request, res: Response, next: NextFunction) => {
-	res.status(HTTPSTATUS.OK).json({
-		message: 'Hello Subscribe to the channel & share',
-	});
-});
+app.get(
+	`/`,
+	asyncHandler((req: Request, res: Response, next: NextFunction) => {
+		throw new Error("Test Error")
+		res.status(HTTPSTATUS.OK).json({
+			message: 'Hello Subscribe to the channel & share',
+		});
+	})
+);
 
 app.listen(config.PORT, async () => {
 	console.log(
 		`Server listening on port ${config.PORT} in ${config.NODE_ENV}`
-  );
-  await connectDatabase();
+	);
+	await connectDatabase();
 });
