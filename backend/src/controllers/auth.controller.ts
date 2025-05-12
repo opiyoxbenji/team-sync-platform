3d70302e8a2eccc2d4fc1ee2ba3f8068bb74efd0
pixelbushi <opiyosbenji@gmail.com>
2025-05-12 12:56:26 +0300

    created a register user service

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,6 +1,8 @@
 import { asyncHandler } from '../middlewares/asynchandler.middleware';
 import { Request, Response } from 'express';
 import { config } from '../config/app.config';
+import { registerSchema } from '../validation/auth.validation';
+import { HTTPSTATUS } from '../config/http.config';
 
 export const googleLoginCallback = asyncHandler(
 	async (req: Request, res: Response) => {
@@ -15,5 +17,19 @@ export const googleLoginCallback = asyncHandler(
 		return res.redirect(
 			`${config.FRONTEND_ORIGIN}/workspace/${currentWorkspace}`
 		);
+	}
+);
+
+export const registerUserController = asyncHandler(
+	async (req: Request, res: Response) => {
+		const body = registerSchema.parse({
+			...req.body,
+		});
+
+		await registerUserService(body);
+
+		return res.status(HTTPSTATUS.CREATED).json({
+			message: 'User created successfully',
+		});
 	}
 );
--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -4,8 +4,9 @@ import AccountModel from '../models/account.model';
 import WorkspaceModal from '../models/workspace.model';
 import RoleModel from '../models/roles-permission.model';
 import { Roles } from '../enums/role.enum';
-import { NotFoundException } from '../utils/appError';
+import { BadRequestException, NotFoundException } from '../utils/appError';
 import MemberModel from '../models/member.model';
+import { ProviderEnum } from '../enums/account-provider.enum';
 
 export const loginOrCreateAccountService = async (data: {
 	provider: string;
@@ -66,13 +67,85 @@ export const loginOrCreateAccountService = async (data: {
 			await user.save({ session });
 		}
 		await session.commitTransaction();
-    session.endSession();
-    console.log("End session .....");
-  } catch (error) {
-    await session.abortTransaction();
-    session.endSession();
-    throw error;
-  } finally {
-    session.endSession();
-  }
+		session.endSession();
+		console.log('End session .....');
+	} catch (error) {
+		await session.abortTransaction();
+		session.endSession();
+		throw error;
+	} finally {
+		session.endSession();
+	}
+};
+
+export const registerUserService = async (body: {
+	email: string;
+	name: string;
+	password: string;
+}) => {
+	const { email, name, password } = body;
+
+	const session = await mongoose.startSession();
+
+	try {
+		const existingUser = await UserModel.findOne({ email }).session(
+			session
+		);
+		if (existingUser) {
+			throw new BadRequestException('Email already exists');
+		}
+
+		const user = new UserModel({
+			email,
+			name,
+			password,
+		});
+		await user.save({ session });
+
+		const account = new AccountModel({
+			userId: user._id,
+			provider: ProviderEnum.EMAIL,
+			providerId: email,
+		});
+		await account.save({ session });
+
+		const workspace = new WorkspaceModal({
+			name: 'My workspace',
+			owner: user._id,
+			description: `Workspace created for ${user.name}`,
+		});
+		await workspace.save({ session });
+
+		const ownserRole = await RoleModel.findOne({
+			name: Roles.OWNER,
+		}).session(session);
+
+		if (!ownserRole) {
+			throw new NotFoundException('Owner role not found');
+		}
+
+		const member = new MemberModel({
+			user: user._id,
+			workspaceId: workspace._id,
+			role: ownserRole._id,
+			joinedAt: new Date(),
+		});
+		await member.save({ session });
+
+		user.currentWorkspace = workspace._id as mongoose.Types.ObjectId;
+		await user.save({ session });
+
+		await session.commitTransaction();
+		session.endSession();
+		console.log('End Session...');
+
+		return {
+			userId: user._id,
+			workspaceId: workspace._id,
+		};
+	} catch (error) {
+		await session.abortTransaction();
+		session.endSession();
+		throw error;
+	}
 };
